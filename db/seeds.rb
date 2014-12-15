21ac27f865fd7cf98f48abe7ccd9f09cb122ba76
root <rawee.mbe8@gmail.com>
2014-12-15 02:20:19 +0000

    push from server

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,8 @@ end
 Province::ARRS.each do |p|
 	pv = Province.find_or_create_by(:name => p)
 end
+
+user = User.where(['email = ?', 'admin@npproperty.net']).first
+user = User.new(:email => 'admin@npproperty.net') if user.nil?
+user.password = "NpPr0p3rtii$!"
+user.save
